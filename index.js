7c1c7d1a7b3d5bbe9e1a9f73eb8aa5930498e818
Kate Liptak <kate.liptak@gmail.com>
2022-06-24 10:01:44 -0400

    solved

--- index.js
+++ index.js
@@ -34,11 +34,11 @@ document.onkeyup = function(e){
 // #Should not have menu open when page loads - auto passes √
 // #Should open menu when clicking the hamburger button √
 // #Should set aria-expanded="true" to hamburger button when menu is opened √
-// #Should close menu when clicking the hamburger button
+// #Should close menu when clicking the hamburger button √
 // #Should set aria-expanded="false" to hamburger button when menu is closed √
 // #Should NOT open menu when clicking things other than the hamburger button - auto passes √
-// #Should close menu when clicking outside of the menu
+// #Should close menu when clicking outside of the menu √
 // #Should NOT close menu when clicking inside of the menu - auto passes √
-// #Should close menu when pressing escape and menu items are NOT focused
-// #Should close menu and focus hamburger button when pressing escape and menu items are focused
+// #Should close menu when pressing escape and menu items are NOT focused √
+// #Should close menu and focus hamburger button when pressing escape and menu items are focused √
 // #Should NOT open menu when pressing escape - auto passes √
